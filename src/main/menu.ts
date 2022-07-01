1bd35eb04434d47bf86547bbd8be5f79b19f695d
mazizkurt <aziz.app.developer@gmail.com>
2022-07-01 14:16:36 +0300

    updated build

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Birikim',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Birikim',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
